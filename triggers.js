a3c43f29105e20edc29b18c6fd9310591837ff02
Matej Plavevski <matej.plavevski@dobij5.com>
2017-03-04 10:13:19 +0100

    Update triggers.js

--- triggers.js
+++ triggers.js
@@ -1,1 +1,7 @@
+module.export = {
 
+search: [ "get a girlfriend","get a boyfriend","get a date", "get a prom"],
+info : ["help", "info", "commands", "instructions"],
+love : ["do you like me", "do you love me", "will you be my valentines", "can i marry you", "will you marry me"]
+
+}

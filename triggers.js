294330e80d718ef41103929dda52fa4f8714d54a
Matej Plavevski <matej.plavevski@dobij5.com>
2017-03-08 17:46:48 +0100

    Too tired to write descriptive message

--- triggers.js
+++ triggers.js
@@ -1,7 +1,9 @@
-module.export = {
+var triggers = {
 
 search: [ "get a girlfriend","get a boyfriend","get a date", "get a prom"],
 info : ["help", "info", "commands", "instructions"],
 love : ["do you like me", "do you love me", "will you be my valentines", "can i marry you", "will you marry me"]
 
 }
+
+module.exports = triggers;

7bad8b9b69ac06bafbae9e32f449d507327eff74
Matej Plavevski <matej.plavevski@dobij5.com>
2017-03-08 17:46:00 +0100

    Too tired to write descriptive message

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-module.exports = {
+var config = {
 
 token: "", //Token for the Slack Bot
 admin: "", //This is the user ID. Used for the exit command currently
-weathertoken: "" //From OpenWeatherMap
-  
+weathertoken: "", //From OpenWeatherMap
+app_id: "", // App ID From Oxford Dictionaries
+app_key: "", // App Key From Oxford Dictionaries
+language: "en" // Language for Oxford Dictionaries 
 }
+
+module.exports = config;

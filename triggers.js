9a0deb15c47c64ae55aaa959a5fdafeab90727c2
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-04-17 12:34:40 +0200

    Update triggers.js

--- triggers.js
+++ triggers.js
@@ -3,7 +3,7 @@ var triggers = {
 search: [ "get a girlfriend","get a boyfriend","get a date", "get a prom"],
 info : ["help", "info", "commands", "instructions"],
 love : ["do you like me", "do you love me", "will you be my valentines", "can i marry you", "will you marry me"]
-
+stats : ["stats", "statistics"]
 }
 
 module.exports = triggers;

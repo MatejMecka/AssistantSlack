aac81174f94c43a576e30d04dc4139ae88f1cff4
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-05-18 20:23:01 +0200

    Soon...

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ weathertoken: "", //From OpenWeatherMap
 app_id: "", // App ID From Oxford Dictionaries
 app_key: "", // App Key From Oxford Dictionaries
 language: "en" // Language for Oxford Dictionaries 
+cse: "" //
+cseapi: "" //  
 }
 
 module.exports = config;

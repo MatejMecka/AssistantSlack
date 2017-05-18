a37f5e7e7c7038141693c751b517884b62b0dc00
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-05-18 21:52:04 +0200

    Added Google API fields

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ admin: "", //This is the user ID. Used for the exit command currently
 weathertoken: "", //From OpenWeatherMap
 app_id: "", // App ID From Oxford Dictionaries
 app_key: "", // App Key From Oxford Dictionaries
-language: "en" // Language for Oxford Dictionaries 
-cse: "" //
-cseapi: "" //  
+language: "en", // Language for Oxford Dictionaries 
+cse: "", // Google Custom Search Engine Key.
+cseapi: "" // Google Search API Key.
 }
 
 module.exports = config;

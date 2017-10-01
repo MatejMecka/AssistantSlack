586d40f13d0cfbe902b3fd49d35ba4094a304ae4
Victor Truong <me@ifvictr.com>
2017-10-01 22:20:59 +0000

    Code cleanup

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
-var config = {
-
-token: "", //Token for the Slack Bot
-admin: "", //This is the user ID. Used for the exit command currently
-weathertoken: "", //From OpenWeatherMap
-app_id: "", // App ID From Oxford Dictionaries
-app_key: "", // App Key From Oxford Dictionaries
-language: "en", // Language for Oxford Dictionaries 
-cse: "", // Google Custom Search Engine Key.
-cseapi: "", // Google Search API Key.
-gifapi: "" // Tenor API Key  
-}
+const config = {
+    token: "", //Token for the Slack Bot
+    admin: "", //This is the user ID. Used for the exit command currently
+    weathertoken: "", //From OpenWeatherMap
+    app_id: "", // App ID From Oxford Dictionaries
+    app_key: "", // App Key From Oxford Dictionaries
+    language: "en", // Language for Oxford Dictionaries 
+    cse: "", // Google Custom Search Engine Key.
+    cseapi: "", // Google Search API Key.
+    gifapi: "" // Tenor API Key  
+};
 
 module.exports = config;
